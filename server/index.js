9d337e1b91ebe71efa5315da15e0e3f14523d59a
shady2532 <shadyrizk2532@gmail.com>
2024-10-27 03:25:58 +0300

    packages

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (request, response)=>{
+    console.log(`Server running on port: ${port}`);
+}
+
+);
